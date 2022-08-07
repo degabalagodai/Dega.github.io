b350ce816224332a94b794ba050454102045f8e6
degabalagodai <92358925+degabalagodai@users.noreply.github.com>
2022-08-07 13:35:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var nama = prompt("Siapa nama kamu?");
+var usia = prompt("Berapa usia kamu?");
+var search = prompt("nyari apa hayyo kesini?");
+var hard = prompt("JS masih belajar ,kasih tips belajar yaa");
+
+
+document.write("Hello "+ nama + "usia" + usia + "search" + search + "hard");
+
+alert("semangat dan berbahagia");
+
